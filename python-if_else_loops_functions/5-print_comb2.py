9bb0cd6dec5e6f05f0ed3d7daa5831b15cb74087
Planisph3r3 <ennzopz@gmail.com>
2023-09-16 18:49:58 -0700

    Printing numbers from 0 to 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for k in range(10):
-        if i == 9 and k == 9:
-            print("{:d}{:d}".format(i, k))
-        else:
-            print("{:d}{:d}".format(i, k), end=", ")
+for i in range(100):
+    if i == 99:
+        print("{:02d}".format(i))
+    else:
+        print("{:02d}".format(i), end=", ")
