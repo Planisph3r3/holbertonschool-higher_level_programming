0b907dc696c246e26ebc0042efee7e29efe4d069
Planisph3r3 <ennzopz@gmail.com>
2023-09-17 14:59:14 -0700

    Prints a string in uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,9 @@ def uppercase(str):
     fuli = ""
     for i in str:
         k = (ord(i) - 32)
-        l = chr(k)
-        if "A" <= i <= "Z" or i == " " or "0" <= i <= "9":
+        upper = chr(k)
+        if "A" <= i <= "Z" or i == " " or "0" <= i <= "9" or i == ",":
             fuli += i
         else:
-            fuli += l
+            fuli += upper
     print("{:s}".format(fuli))
