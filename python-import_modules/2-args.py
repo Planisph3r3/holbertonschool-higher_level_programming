eed24f6a6322c5ed584a5d04a6305bd18717f2f6
Planisph3r3 <ennzopz@gmail.com>
2023-09-21 21:58:41 -0700

    Prints the number of and the list of its arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 import sys
-arg = len(sys.argv)
-for i in range(arg):
-    if i == arg:
-        break
 
-if i == 1:
-    print("{:d} argument:".format(i)) 
-elif i == 0:
-    print("{:d} arguments.".format(i)) 
+if len(sys.argv) == 2:
+    print("1 argument:") 
+elif len(sys.argv) == 1:
+    print("{} arguments.".format(len(sys.argv)-1)) 
 else:
-    print("{:d} arguments:".format(i))
+    print("{} arguments:".format(len(sys.argv)-1))
 
 for i in range(1, len(sys.argv)):
     k = sys.argv[i]
