b34c60dbbb376412953bfabae5e8ccdb59c65239
Planisph3r3 <ennzopz@gmail.com>
2023-09-21 22:04:29 -0700

    Fixing the code adding guard

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import sys
+def main()
+    if len(sys.argv) == 2:
+        print("1 argument:") 
+    elif len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv)-1)) 
+    else:
+        print("{} arguments:".format(len(sys.argv)-1))
 
-if len(sys.argv) == 2:
-    print("1 argument:") 
-elif len(sys.argv) == 1:
-    print("{} arguments.".format(len(sys.argv)-1)) 
-else:
-    print("{} arguments:".format(len(sys.argv)-1))
-
-for i in range(1, len(sys.argv)):
-    k = sys.argv[i]
-    print(f"{i}: {k}")
+    for i in range(1, len(sys.argv)):
+        k = sys.argv[i]
+        print(f"{i}: {k}")
+if __name__ == "__main__":
+    main()
