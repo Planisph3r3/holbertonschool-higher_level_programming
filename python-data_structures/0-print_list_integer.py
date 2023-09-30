7228bf09e0002d22015852bba68ddf3f5af02b69
Planisph3r3 <ennzopz@gmail.com>
2023-09-29 18:46:35 -0700

    Prints the members of a list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def replace_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return my_list
