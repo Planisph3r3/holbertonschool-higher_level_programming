e14b70435afc6da469ddd268df37629832889bae
Planisph3r3 <ennzopz@gmail.com>
2023-10-03 15:17:57 -0700

    Using private attributes on instances

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """The square itself"""
+
+    def __init__(self, size):
+        """Init of the method which builds a new square
+        Args:
+            size (int): Size of the new square
+        """
+        self.__size = size
