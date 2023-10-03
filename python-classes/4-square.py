0480b973a6d394f0da4e7f59815e17853fba719a
Planisph3r3 <ennzopz@gmail.com>
2023-10-03 15:58:24 -0700

    Adding a getter and a setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """The square itself
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if len(value) > 0:
+            self.__size = value
+
+    def area(self):
+        return self.__size**2
