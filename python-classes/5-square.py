ecc43fe66c1b0f89aa943d8ad09e465e791e91cc
Planisph3r3 <ennzopz@gmail.com>
2023-10-07 14:45:51 -0700

    Add: Prints in stdout the square with the character #

---
+++ python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """The square itself.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter & Setter for the size value of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Getter & Setter for the size value of the square."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        "Returns the area of the square"
+        return self.__size**2
+    
+    def my_print(self):
+        if self.size == 0:
+            print()
+        for rows in range(self.size):
+            print("#" * self.size)
