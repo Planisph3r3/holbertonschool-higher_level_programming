6feaa5a06d484bb30dd347c4fc19817695472804
Planisph3r3 <ennzopz@gmail.com>
2023-10-11 21:43:31 -0700

    Trying some foreign code to make sure some validations

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,44 +1,44 @@
 #!/usr/bin/python3
-"""Define a Rectangle."""
+"""Este módulo define la clase Rectangle."""
 
 
 class Rectangle:
-    """The Rectangle itself"""
+    """Esta clase expone un Rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Instantiation with optional width and height
+        """Método constructor, tiene 2 parámetros opcionales.
 
         Args:
-                width (int): width of the rectangle
-                height (int): height of the rectangle
+            width (int): El ancho del Rectangle.
+            height (int): El alto del Rectangle.
         """
-        self.__height = height
-        self.__width = width
-
-    @property
-    def height(self):
-        """Getter & Setter for the attribute height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Getter & Setter for the attribute height"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """Getter & Setter for the attribute width"""
+        """Este método retorna el ancho de nuestro Rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Getter & Setter for the attribute width"""
+        """Este propiedad configura el ancho"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """Este método retorna el alto de  nuestro Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Esta propiedad configura el alto"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
