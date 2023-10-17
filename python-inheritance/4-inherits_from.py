07f09c0b219588ac4c603d19c71cc5a8e7f5b328
Planisph3r3 <ennzopz@gmail.com>
2023-10-17 16:03:50 -0700

    Checking objects with issubclass()

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a module for checking the instance of an object that inherited directly from the specified class"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True or False depending if obj is exactly the same instance
+    as the specified"""
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
