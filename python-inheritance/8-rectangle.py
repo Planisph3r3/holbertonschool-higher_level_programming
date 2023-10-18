3471050dcfc6705fe45db0668cec6497a398a841
Planisph3r3 <ennzopz@gmail.com>
2023-10-17 17:39:57 -0700

    Fix: Pycodestyle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,9 +4,11 @@
 
 class BaseGeometry:
     """An empty class no more"""
+
     def area(self):
         """Module that raises an exception"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """Checks if value is valid"""
         if type(value) is not int:
@@ -14,9 +16,13 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(name + " must be greater than 0")
 
+
 """Defining a class Rectangle"""
+
+
 class Rectangle(BaseGeometry):
     """A class that uses another one as template"""
+
     def __init__(self, width, height):
         """Instantiation of width and height"""
         self.integer_validator("width", width)
@@ -25,4 +31,6 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def integer_validator(self, name, value):
+        """Checks if value is valid"""
         super().integer_validator(name, value)
+
