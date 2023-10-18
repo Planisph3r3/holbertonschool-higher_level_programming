1df0b94f30aca0cb5baa69086e21ed8ce95133da
Planisph3r3 <ennzopz@gmail.com>
2023-10-17 17:58:57 -0700

    Added an area and __str__ function to the subclass

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Define a class"""
+
+
+class BaseGeometry:
+    """An empty class no more"""
+
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Checks if value is valid"""
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+
+
+"""Defining a class Rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """A class that uses another one as template"""
+
+    def __init__(self, width, height):
+        """Instantiation of width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def integer_validator(self, name, value):
+        """Checks if value is valid"""
+        super().integer_validator(name, value)
+    
+    def area(self):
+        """Returns the area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+        
