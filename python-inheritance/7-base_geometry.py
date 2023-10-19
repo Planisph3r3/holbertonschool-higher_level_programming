536ad9b66ef9ad8af13733c86733ac3486cb9415
Planisph3r3 <ennzopz@gmail.com>
2023-10-19 10:52:09 -0700

    Pycodestyle fix

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,9 +4,11 @@
 
 class BaseGeometry:
     """An empty class no more"""
+
     def area(self):
         """Module that raises an exception"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """Checks if value is valid"""
         if type(value) is not int:
