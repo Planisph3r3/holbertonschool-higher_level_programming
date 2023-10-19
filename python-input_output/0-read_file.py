ef21c0063df76b04aa72116d206c4e2d62447331
Planisph3r3 <ennzopz@gmail.com>
2023-10-19 16:08:20 -0700

    Fix: Missing documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defining a function that reads and prints a whole text file"""
+
+
 def read_file(filename="", encoding="utf-8"):
     """Printing the content of a file to stout"""
     with open(filename) as f:
