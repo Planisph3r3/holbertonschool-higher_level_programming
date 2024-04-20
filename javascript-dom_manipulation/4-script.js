c7ba8aab26112cb2bb2d9a5afbbb0fa209f798d9
Planisph3r3 <ennzopz@gmail.com>
2024-04-19 20:13:00 -0500

    Fix: Added text for the appended element

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -6,6 +6,7 @@ sel_id.addEventListener("click", function() {
 
         const sel_list = document.querySelector('ul.my_list');
         const list_mem = document.createElement('li');
+        list_mem.textContent = 'Item'
         sel_list.appendChild(list_mem);
     }
     add_item();
